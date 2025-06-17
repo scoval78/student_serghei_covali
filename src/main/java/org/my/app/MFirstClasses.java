26450d33aa38fccb66679d46ee4f61484136afe4
Коваль СергейЭgit config --global user.name Коваль <scoval@mail.ru>
2025-06-17 11:03:43 +0300

    Merge remote-tracking branch 'origin/main'

--- src/main/java/org/my/app/MFirstClasses.java
+++
@@ -1,7 +0,0 @@
-package org.my.app;
-
-public class MFirstClasses {
-    public static void main(String[] args) {
-        System.out.print("Привет, Сергей!");
-    }
-}
--- src/main/java/org/my/app/MyFirstClass.java
+++ src/main/java/org/my/app/MyFirstClass.java
@@ -4,5 +4,6 @@ public class MyFirstClass {
 
     public static void main(String[] args) {
         System.out.print("Hello, world!");
+        System.out.print("Привет, Сергей!");
     }
 }
